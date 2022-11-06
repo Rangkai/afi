0554ef2c85285889d3381a0d2b7c1cf22c49e254
mhafizd21 <maulanahafizd21@gmail.com>
2022-11-06 23:02:26 +0700

    create 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,35 @@
+import {
+  Flex, Container, Text, Button,
+} from '@chakra-ui/react';
+import { Link } from 'gatsby';
+import React from 'react';
+import Layout from '../components/Layout';
+import SEO from '../components/Seo';
+
+function NotFound404() {
+  return (
+    <Layout>
+      <Container minH="calc(100vh - 342px)">
+        <Flex justifyContent="center" minH="calc(100vh - 342px)" alignItems="center" flexDirection="column">
+          <Text
+            color="brandRed.500"
+            fontSize={['24px', null, '32px']}
+            marginBottom="16px"
+            fontWeight="600"
+          >
+            Maaf halaman tidak ditemukan
+          </Text>
+          <Button variant="ghost" as={Link} to="/" colorScheme="brandRed">
+            Kembali ke Beranda
+          </Button>
+        </Flex>
+      </Container>
+    </Layout>
+  );
+}
+
+export default NotFound404;
+
+export function Head() {
+  return <SEO />;
+}
